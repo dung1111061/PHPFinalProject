26da5dcdf1a625af460983ed565ad2ac13bb0634
Nguyen Dung <dung1111061@gmail.com>
2020-01-09 22:03:37 +0700

    Add field new flag, top selling flag for product table and add category table

--- quanlibanhang_offical.sql
+++ quanlibanhang_offical.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2019 at 03:27 PM
+-- Generation Time: Jan 09, 2020 at 03:56 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -31,16 +31,59 @@ SET time_zone = "+00:00";
 CREATE TABLE `admin` (
   `id` int(5) NOT NULL,
   `username` varchar(20) CHARACTER SET utf8 NOT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `real_name` varchar(100) NOT NULL,
   `password` varchar(40) CHARACTER SET utf8 NOT NULL,
-  `email` varchar(25) CHARACTER SET utf8 NOT NULL
+  `email` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `privilege` int(1) NOT NULL,
+  `avatar` varchar(50) CHARACTER SET utf8 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `password`, `email`) VALUES
-(1, 'admin', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'dung1111061@gmail.com');
+INSERT INTO `admin` (`id`, `username`, `first_name`, `last_name`, `real_name`, `password`, `email`, `privilege`, `avatar`) VALUES
+(23, 'admin', 'Dung', 'Nguyen', 'Nguyen Dung', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'nguyendung261294@gmail.com', 2, 'profile-pic.jpg'),
+(25, 'user', 'Hieu', 'Nguyen', 'Nguyen Trung Hieu', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'dung1111061@gmail.com', 1, ''),
+(27, 'user_testing', 'Hieu', 'Nguyen', 'Nguyen Trung Hieu 2', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'nguyendung261295@gmail.com', 0, ''),
+(28, 'admin_testing', '', '', '', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'nguyendung261296@gmail.com', 0, '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(5) NOT NULL,
+  `name` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conversation`
+--
+
+CREATE TABLE `conversation` (
+  `id` int(11) NOT NULL,
+  `bodymsg` varchar(500) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `conversation`
+--
+
+INSERT INTO `conversation` (`id`, `bodymsg`, `user`, `time`) VALUES
+(72, 'new comment', 'admin', '2020-01-09 21:32:03'),
+(73, 'comment lai ne', 'admin', '2020-01-09 21:32:10'),
+(74, 'comment lai nua ne', 'admin', '2020-01-09 21:32:28'),
+(75, 'comment thanh cong roi ne', 'admin', '2020-01-09 21:32:35'),
+(76, 'lai', 'user', '2020-01-09 21:35:53');
 
 -- --------------------------------------------------------
 
@@ -210,11 +253,32 @@ CREATE TABLE `order_status` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `privilege`
+--
+
+CREATE TABLE `privilege` (
+  `privilege` int(1) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `privilege`
+--
+
+INSERT INTO `privilege` (`privilege`, `description`) VALUES
+(0, 'No Active'),
+(1, 'demonstrator'),
+(2, 'Administrator');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
+  `category_id` int(3) DEFAULT NULL,
   `description_id` int(11) NOT NULL DEFAULT '0',
   `description` varchar(500) DEFAULT NULL,
   `language_id` int(11) NOT NULL DEFAULT '1',
@@ -233,15 +297,28 @@ CREATE TABLE `product` (
   `status` tinyint(1) NOT NULL DEFAULT '0',
   `viewed` int(5) NOT NULL DEFAULT '0',
   `date_added` datetime NOT NULL,
-  `date_modified` datetime NOT NULL
+  `date_modified` datetime NOT NULL,
+  `new_flag` int(1) NOT NULL DEFAULT '0' COMMENT '>=1 new product',
+  `top_selling_flag` int(1) NOT NULL DEFAULT '0' COMMENT '>=1  top selling product',
+  `percentage_discount` int(3) NOT NULL DEFAULT '0' COMMENT '10 = discount 10%'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`product_id`, `description_id`, `description`, `language_id`, `product_name`, `model`, `quantity`, `image`, `manufacturer_id`, `shipping`, `price`, `date_available`, `weight`, `length`, `width`, `height`, `status`, `viewed`, `date_added`, `date_modified`) VALUES
-(87, 0, 'pseudo description', 1, 'Iphone 11 Promax 64GB', 'Iphone', NULL, '', NULL, 1, NULL, '2019-12-14', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `product` (`product_id`, `category_id`, `description_id`, `description`, `language_id`, `product_name`, `model`, `quantity`, `image`, `manufacturer_id`, `shipping`, `price`, `date_available`, `weight`, `length`, `width`, `height`, `status`, `viewed`, `date_added`, `date_modified`, `new_flag`, `top_selling_flag`, `percentage_discount`) VALUES
+(36, NULL, 0, '<p>pseudo description</p>\r\n', 1, 'dell vostro 2', '', NULL, '1578471260_2.jpg', 3, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(40, NULL, 0, '<p>pseudo description</p>\r\n', 1, 'Iphone 11 Promax 64GB', '', NULL, '1578472349_3.jpg', 1, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(50, NULL, 0, '										\r\n								', 1, 'test 7', '', NULL, '1578574486_2.jpg', NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(51, NULL, 0, NULL, 1, 'test 8', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(52, NULL, 0, NULL, 1, 'test 9', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(53, NULL, 0, NULL, 1, 'test 10', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(54, NULL, 0, NULL, 1, 'test 11', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(55, NULL, 0, NULL, 1, 'test 12', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(56, NULL, 0, NULL, 1, 'test 13', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(57, NULL, 0, NULL, 1, 'test 14', NULL, NULL, NULL, NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0),
+(58, NULL, 0, '									pseudo description	\r\n								', 1, 'test 100', '', NULL, '1578574524_3.jpg', NULL, 1, NULL, '0000-00-00', '0.00000000', '0.00000000', '0.00000000', '0.00000000', 0, 0, '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -284,7 +361,21 @@ CREATE TABLE `task` (
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `privilege` (`privilege`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `currency`
@@ -342,6 +433,12 @@ ALTER TABLE `order_status`
   ADD PRIMARY KEY (`order_status_id`,`language_id`);
 
 --
+-- Indexes for table `privilege`
+--
+ALTER TABLE `privilege`
+  ADD PRIMARY KEY (`privilege`);
+
+--
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
@@ -349,7 +446,8 @@ ALTER TABLE `product`
   ADD UNIQUE KEY `product_name` (`product_name`),
   ADD KEY `fk_manufacture` (`manufacturer_id`),
   ADD KEY `language_id` (`language_id`),
-  ADD KEY `description_id` (`description_id`);
+  ADD KEY `description_id` (`description_id`),
+  ADD KEY `category_id` (`category_id`);
 
 --
 -- Indexes for table `review`
@@ -370,6 +468,24 @@ ALTER TABLE `task`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `conversation`
+--
+ALTER TABLE `conversation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT for table `currency`
@@ -423,7 +539,7 @@ ALTER TABLE `order_status`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `review`
@@ -440,6 +556,18 @@ ALTER TABLE `task`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`privilege`) REFERENCES `privilege` (`privilege`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `conversation`
+--
+ALTER TABLE `conversation`
+  ADD CONSTRAINT `conversation_ibfk_1` FOREIGN KEY (`user`) REFERENCES `admin` (`username`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `description`
@@ -468,7 +596,8 @@ ALTER TABLE `order_product`
 ALTER TABLE `product`
   ADD CONSTRAINT `fk_manufacture` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturer` (`manufacturer_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`description_id`) REFERENCES `description` (`id`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `product_ibfk_2` FOREIGN KEY (`description_id`) REFERENCES `description` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `review`
@@ -476,13 +605,6 @@ ALTER TABLE `product`
 ALTER TABLE `review`
   ADD CONSTRAINT `review_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `review_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `task`
---
-ALTER TABLE `task`
-  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`created_admin_id`) REFERENCES `admin` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `task_ibfk_2` FOREIGN KEY (`assigned_admin_id`) REFERENCES `admin` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
